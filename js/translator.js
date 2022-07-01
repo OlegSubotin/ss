97357ad696a51f0053d94d39348b360ce64d141e
OlegSubotin <subotinoleg0@gmail.com>
2022-07-01 11:48:14 +0300

    translation for home page

---
+++ js/translator.js
@@ -0,0 +1,29 @@
+const selectEl = document.querySelector('.js-change-lang');
+const allLang = ['ru', 'ua'];
+
+selectEl.addEventListener('change', changeUrlLanguage);
+
+function changeUrlLanguage() {
+    let lang = selectEl.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ua';
+        location.reload();
+    };
+    selectEl.value = hash;
+    
+    for (let key in langArr) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langArr[key][hash];
+        };
+    };
+}
+
+changeLanguage();
